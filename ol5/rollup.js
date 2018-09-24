7ecea70fdd79659697bb7c849ee484ffddbb80d2
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2018-09-24 15:41:37 +0200

    Generate both minified and unminified builds

--- ol5/rollup.js
+++ ol5/rollup.js
@@ -6,21 +6,31 @@ import {uglify} from 'rollup-plugin-uglify';
 import buble from 'rollup-plugin-buble';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 
+const prod = process.env.BUILD === 'production';
+
+const plugins = [
+  noderesolve(),
+  commonjs(),
+  buble(),
+];
+
+const uglifyPlugin = uglify({
+    compress: {
+      sequences: false, // workaround uglify bug with sequences
+      drop_console: true
+    }
+  });
+
+if (prod) {
+  plugins.push(uglifyPlugin);
+}
+
+plugins.push(sourcemaps());
+
 export default {
   input: 'build/index.js',
   output: [
-    {file: 'build/ol.js', format: 'iife', sourcemap: true}
+    {file: prod ? 'build/ol.js' : 'build/ol-debug.js', format: 'iife', sourcemap: true}
   ],
-  plugins: [
-    noderesolve(),
-    commonjs(),
-    buble(),
-    uglify({
-      compress: {
-        sequences: false, // workaround uglify bug with sequences
-        drop_console: true
-      }
-    }),
-    sourcemaps()
-  ]
+  plugins
 };
